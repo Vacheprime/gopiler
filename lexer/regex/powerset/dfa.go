3dfcdee1cfa08731f253e27cec2b044e8d770cf3
Vacheprime <danatmuradov500@gmail.com>
2026-05-28 13:03:15 -0400

    WIP

--- lexer/regex/powerset/dfa.go
+++ lexer/regex/powerset/dfa.go
@@ -1,1 +1,20 @@
 package powerset
+
+import (
+	gp "github.com/Vacheprime/gopiler"
+	gl "github.com/Vacheprime/gopiler/lexer/regex/glushkov"
+)
+
+type DFA struct {
+	Classifier  gl.SymbolClassifier
+	Transitions [][]int // A transition only has one next state
+	FinalStates gp.BitSet
+}
+
+func BuildDFA(nfa gl.NFA) DFA {
+	// Reusing the NFA classifier as it is identical
+	dfa := DFA{Classifier: nfa.Classifier}
+	dfaTransitions := make([][]int, 0)
+
+	return dfa
+}
