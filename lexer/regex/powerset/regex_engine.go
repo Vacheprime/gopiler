e85fe8d30aba2eed83bb58ead03fa2f6a14d03dd
Danat Ali Muradov <danatmuradov500@gmail.com>
2026-08-19 17:40:36 -0400

    Added comments and removed old Match function.

--- lexer/regex/powerset/regex_engine.go
+++ lexer/regex/powerset/regex_engine.go
@@ -170,40 +170,14 @@ func (m *DFAMatcher) MatchNext() (nextMatch ReMatch, ok bool) {
 	return nextMatch, nextMatch.EndIndex != -1
 }
 
+// advanceRunePosition moves the rune position forward by 1.
 func (m *DFAMatcher) advanceRunePosition() {
 	m.runePosition++
 }
 
+// rewindRunes adds runes to the replayReader and decrements the
+// runePosition to match the rewind.
 func (m *DFAMatcher) rewindRunes(runes []rune) {
 	m.runePosition -= len(runes)
 	m.replayReader.ReplayRunes(runes)
-}
-
-func Match(search string, dfa DFA) (match ReMatch, hasMatch bool) {
-	chars := []rune(search)
-	state := 0
-	latestMatch := ReMatch{0, -1, "", nil}
-	if dfa.FinalStates.Bits[0]&1<<0 != 0 {
-		latestMatch.EndIndex = 0
-	}
-	for i, c := range chars {
-		charClass := dfa.Classifier.Classify(c)
-		if charClass == -1 {
-			break
-		}
-		nextState := dfa.Transitions[state][charClass]
-		if nextState == -1 {
-			break
-		}
-		state = nextState
-		// Found result
-		if dfa.FinalStates.IsSet(uint(state)) {
-			latestMatch.EndIndex = i
-			latestMatch.Labels = dfa.FinalStateLabels[state]
-		}
-	}
-	if latestMatch.EndIndex != -1 {
-		latestMatch.Match = string(chars[latestMatch.StartIndex : latestMatch.EndIndex+1])
-	}
-	return latestMatch, latestMatch.EndIndex != -1
 }
