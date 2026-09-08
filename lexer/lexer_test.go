4db2ad0b4227be612d6393b3c65cc74c61c1b648
Vacheprime <danatmuradov500@gmail.com>
2026-09-08 18:57:11 -0400

    Implemented tests for failure cases and subsequent EOFs.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"errors"
 	"io"
 	"testing"
 
@@ -60,6 +61,11 @@ var (
 type matchPair struct {
 	match pw.ReMatch
 	err   error
+}
+
+type tokenResult struct {
+	tk  Token
+	err error
 }
 
 type mockMatcher struct {
@@ -224,6 +230,48 @@ func TestNextToken(t *testing.T) {
 			},
 		},
 		{
+			name: "[Unmatched sequence] ERROR token for unmatched string.",
+			ex:   "abc#",
+			matcher: &mockMatcher{
+				matches: []matchPair{
+					{
+						match: pw.ReMatch{
+							StartIndex: 0,
+							EndIndex:   2,
+							Match:      "abc",
+							Labels:     []string{"IDENTIFIER"},
+							IsMatching: true,
+						},
+						err: nil,
+					},
+					{
+						match: pw.ReMatch{
+							StartIndex: 3,
+							EndIndex:   3,
+							Match:      "#",
+							Labels:     []string{},
+							IsMatching: false,
+						},
+						err: nil,
+					},
+				},
+			},
+			definitions: defs,
+			expectedTokens: []Token{
+				{
+					Repr:   "abc",
+					TkType: IDENTIFIER,
+					Pos:    Position{Line: 0, Col: 0},
+				},
+				{
+					Repr:   "#",
+					TkType: ERROR,
+					Pos:    Position{Line: 0, Col: 3},
+				},
+				{TkType: EOF},
+			},
+		},
+		{
 			name: "[Matcher EOF Contract] Valid match along with EOF.",
 			ex:   "abc",
 			matcher: &mockMatcher{
@@ -312,6 +360,218 @@ func TestNextToken(t *testing.T) {
 				tkGot := result[idx]
 				if tkGot != expTk {
 					t.Errorf("Got token %+v while expecting %+v", tkGot, expTk)
+				}
+			}
+		})
+	}
+}
+
+func TestNextTokenSubsequentEOF(t *testing.T) {
+	const SUBSEQUENT_CALLS int = 3
+	var testCases = []struct {
+		name           string
+		matcher        pw.SequentialMatcher
+		definitions    []Definition
+		expectedTokens []Token
+	}{
+		{
+			name: "[EOF after success] Subsequent EOFs after successful lex.",
+			matcher: &mockMatcher{
+				matches: []matchPair{
+					{
+						match: pw.ReMatch{
+							StartIndex: 0,
+							EndIndex:   1,
+							Match:      "xy",
+							Labels:     []string{"IDENTIFIER"},
+							IsMatching: true,
+						},
+						err: nil,
+					},
+					{
+						match: pw.ReMatch{
+							StartIndex: 2,
+							EndIndex:   2,
+							Match:      "=",
+							Labels:     []string{"ASSIGNMENT"},
+							IsMatching: true,
+						},
+						err: nil,
+					},
+					{
+						match: pw.ReMatch{
+							StartIndex: 3,
+							EndIndex:   3,
+							Match:      "",
+							Labels:     []string{},
+							IsMatching: false,
+						},
+						err: io.EOF,
+					},
+				},
+			},
+			definitions: defs,
+			expectedTokens: []Token{
+				{
+					Repr:   "xy",
+					TkType: IDENTIFIER,
+					Pos:    Position{Line: 0, Col: 0},
+				},
+				{
+					Repr:   "=",
+					TkType: ASSIGNMENT,
+					Pos:    Position{Line: 0, Col: 2},
+				},
+				{TkType: EOF},
+				{TkType: EOF},
+				{TkType: EOF},
+			},
+		},
+		{
+			name: "[EOF after error token] Subsequent EOFs following ERROR token.",
+			matcher: &mockMatcher{
+				matches: []matchPair{
+					{
+						match: pw.ReMatch{
+							StartIndex: 0,
+							EndIndex:   1,
+							Match:      "xy",
+							Labels:     []string{"IDENTIFIER"},
+							IsMatching: true,
+						},
+						err: nil,
+					},
+					{
+						match: pw.ReMatch{
+							StartIndex: 2,
+							EndIndex:   3,
+							Match:      "&^",
+							Labels:     []string{},
+							IsMatching: false,
+						},
+						err: io.EOF,
+					},
+				},
+			},
+			definitions: defs,
+			expectedTokens: []Token{
+				{
+					Repr:   "xy",
+					TkType: IDENTIFIER,
+					Pos:    Position{Line: 0, Col: 0},
+				},
+				{
+					Repr:   "&^",
+					TkType: ERROR,
+					Pos:    Position{Line: 0, Col: 2},
+				},
+				{TkType: EOF},
+				{TkType: EOF},
+				{TkType: EOF},
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			lexer := NewLexer(testCase.matcher, testCase.definitions)
+			result := []Token{}
+			eofCount := 0
+			for eofCount < SUBSEQUENT_CALLS {
+				tk, err := lexer.NextToken()
+				if err != nil {
+					t.Fatalf("Unexpected error %s while expecting none", err)
+				}
+				result = append(result, tk)
+				if tk.TkType == EOF {
+					eofCount++
+				}
+			}
+			nbrExpected := len(testCase.expectedTokens)
+			nbrGot := len(result)
+			if nbrGot != nbrExpected {
+				t.Fatalf("Expected %d tokens, but got %d", nbrExpected, nbrGot)
+			}
+			for idx, expTk := range testCase.expectedTokens {
+				tkGot := result[idx]
+
+				if tkGot != expTk {
+					t.Errorf("Got token %+v while expecting %+v", tkGot, expTk)
+				}
+			}
+		})
+	}
+}
+
+func TestFailedNextToken(t *testing.T) {
+	ErrIO := errors.New("IO error")
+	var testCases = []struct {
+		name           string
+		matcher        pw.SequentialMatcher
+		definitions    []Definition
+		expectedTokens []tokenResult
+	}{
+		{
+			name: "[Invalid Encoding] Invalid encoding error.",
+			matcher: &mockMatcher{
+				matches: []matchPair{
+					{
+						match: pw.ReMatch{},
+						err:   pw.ErrInvalidUTF8Sequence,
+					},
+				},
+			},
+			definitions: defs,
+			expectedTokens: []tokenResult{
+				{
+					tk:  Token{},
+					err: pw.ErrInvalidUTF8Sequence,
+				},
+			},
+		},
+		{
+			name: "[Unexpected error] Unexpected error.",
+			matcher: &mockMatcher{
+				matches: []matchPair{
+					{
+						match: pw.ReMatch{},
+						err:   ErrIO,
+					},
+				},
+			},
+			definitions: defs,
+			expectedTokens: []tokenResult{
+				{
+					tk:  Token{},
+					err: ErrIO,
+				},
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			lexer := NewLexer(testCase.matcher, testCase.definitions)
+			result := []tokenResult{}
+			for {
+				tk, err := lexer.NextToken()
+				tkPair := tokenResult{tk: tk, err: err}
+				result = append(result, tkPair)
+				if tk.TkType == EOF || err != nil {
+					break
+				}
+			}
+			nbrExpected := len(testCase.expectedTokens)
+			nbrGot := len(result)
+			if nbrGot != nbrExpected {
+				t.Fatalf("Expected %d tokens, but got %d", nbrExpected, nbrGot)
+			}
+			for idx, expTk := range testCase.expectedTokens {
+				tkGot := result[idx]
+				if expTk.err != nil && !errors.Is(expTk.err, tkGot.err) {
+					t.Errorf("Got error %s while expecting error %s", tkGot.err, expTk.err)
+				}
+
+				if expTk.err == nil && tkGot.tk != expTk.tk {
+					t.Errorf("Got token %+v while expecting %+v", tkGot.tk, expTk.tk)
 				}
 			}
 		})
